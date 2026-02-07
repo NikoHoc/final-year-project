f212704324b03b340f2b31cadd6c610c856ba8f2
Niko Hoc <niko.owen88@gmail.com>
2026-02-07 14:59:20 +0700

    setup app and supabase client

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+// routes
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// base
+app.get('/', (req, res) => {
+    res.send({
+        status: 'success',
+        message: 'Backend Server is Running...',
+        timestamp: new Date()
+    });
+});
+
+// routes
+app.use('/api/v1/auth', authRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({
+        status: false,
+        message: 'Route not found'
+    });
+});
+
+module.exports = app;
