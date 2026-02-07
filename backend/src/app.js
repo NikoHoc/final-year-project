49324f7781ed0348f9009a505755392cc3516314
Niko Hoc <niko.owen88@gmail.com>
2026-02-07 19:10:48 +0700

    feature - backend: auth module

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // routes
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -10,23 +11,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// base
-app.get('/', (req, res) => {
-    res.send({
-        status: 'success',
-        message: 'Backend Server is Running...',
-        timestamp: new Date()
-    });
+// base route
+app.get("/", (req, res) => {
+  res.send({
+    status: "success",
+    message: "Backend Server is Running...",
+    timestamp: new Date(),
+  });
 });
 
 // routes
 app.use('/api/v1/auth', authRoutes);
 
 app.use((req, res) => {
-    res.status(404).json({
-        status: false,
-        message: 'Route not found'
-    });
+  res.status(404).json({
+    status: false,
+    message: "Route not found",
+  });
 });
 
 module.exports = app;
